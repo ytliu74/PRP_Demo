95135eba90fe5e9afed08f0e2880811a2902402c
Yaotian_Liu <2416224849@qq.com>
2022-04-26 13:16:35 +0800

    update crypto

--- data/crypto_downloader.sh
+++ data/crypto_downloader.sh
@@ -4,8 +4,8 @@
 
 symbols=("BNBUSDT" "BTCUSDT" "ETHUSDT" "XRPUSDT" "SOLUSDT" "LUNAUSDT" "ADAUSDT" "AVAXUSDT" "DOGEUSDT" "DOTUSDT") # add symbols here to download
 # intervals=("1m" "5m" "15m" "30m" "1h" "2h" "4h" "6h" "8h" "12h" "1d" "3d" "1w" "1mo")\
-intervals=("30m" "1h")
-years=("2019" "2020" "2021")
+intervals=("30m" "1h" "2h")
+years=("2021" "2022")
 months=(01 02 03 04 05 06 07 08 09 10 11 12)
 
 baseurl="https://data.binance.vision/data/spot/monthly/klines"
